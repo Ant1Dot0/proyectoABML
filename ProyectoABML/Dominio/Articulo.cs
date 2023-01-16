43b932054ce1088166139b523359f4586d46cfea
Ant1Dot0 <joselicla021@gmail.com>
2023-01-15 21:12:02 -0300

    clase artículo.

---
+++ ProyectoABML/Dominio/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        int id { get; set; }
+        string codigo { get; set; }
+        string descripcion { get; set; }
+        decimal precio { get; set; }
+
+
+    }
+}
